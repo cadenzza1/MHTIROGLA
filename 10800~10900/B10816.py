cf2d0e7128ec8e9f1bf279d24d07a63ad2899260
김민규 <minq0302@naver.com>
2022-03-19 13:43:55 +0900

    commit

--- 10800~10900/B10816.py
+++ 10800~10900/B10816.py
@@ -1,32 +1,18 @@
-n = int(input()) # 가지고 있는 숫자 카드의 개수
-n_num = list(map(int,input().split()))
-n_num.sort()
+n = int(input())
+n_nums = list(map(int,input().split()))
 
 m = int(input())
-m_num = list(map(int,input().split()))
+m_nums = list(map(int,input().split()))
 
-n_len = len(n_num)
+cards = {}
+for i in n_nums:
+    if i in cards:
+        cards[i] += 1
+    else:
+        cards[i] = 1
 
-for i in range(m):
-    left, right = 0, (n_len - 1)
-
-    is_find = False
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if m_num[i] == n_num[mid]:
-            is_find = True
-            print(n_num.count(m_num[i]), end = ' ') # 파이썬 내장함수인 count쓰면 시간초과 떠버림.
-            break
-
-        elif m_num[i] < n_num[mid]:
-            right = mid -1
-            continue
-
-        elif m_num[i] > n_num[mid]:
-            left = mid + 1
-            continue
-
-    if is_find == False:
-        print(0, end= ' ')
+for j in m_nums:
+    if j in cards:
+        print(cards[j], end = ' ')
+    else:
+        print(0, end = ' ')
