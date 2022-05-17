583eb5ce675bc53c40cb50a90f72f848465c3b57
김민규 <minq0302@naver.com>
2022-05-17 10:20:17 +0900

    Solved 2577

--- 1001~2000/1110 더하기사이클.py
+++ 1001~2000/1110 더하기사이클.py
@@ -16,3 +16,19 @@ while True:
 print(cnt)
 
 #2 문자형으로 풀이하는 방법
+"""
+n = input()
+num = n
+cnt = 0
+
+while 1:
+    if len(num) == 1:
+        num = "0" + num
+    plus = str(int(num[0]) + int(num[1]))
+    num = num[-1] + plus[-1]
+    cnt += 1
+    if num == n:
+        print(cnt)
+        break 
+"""
+
