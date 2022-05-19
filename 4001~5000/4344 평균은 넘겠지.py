940d27c865b8720fce25dbf2e3778ad58e375852
김민규 <minq0302@naver.com>
2022-05-19 23:37:50 +0900

    Solved 4344

--- 4001~5000/4344 평균은 넘겠지.py
+++ 4001~5000/4344 평균은 넘겠지.py
@@ -1,15 +1,11 @@
-c = int(input())
+n = int(input())
 
-score = [0] * c
-pyunggyun = [0] * c
-
-for i in range(c):
-    score[i] = list(map(int,input().split()))
-
-for j in range(c):
-    sum = 0
-    for k in range(1,len(score[j])):
-        sum += score[j][k]
-        if k == len(score[j]-1):
-            pyunggyun[j] = sum / score[j][0]
-            
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    avg = sum(nums[1:])/nums[0]  # 평균을 구함 (nums[0]: 학생수, nums[1:] 점수)
+    cnt = 0
+    for score in nums[1:]:
+        if score > avg:
+            cnt += 1  # 평균 이상인 학생 수
+    rate = cnt/nums[0] *100
+    print(f'{rate:.3f}%')
