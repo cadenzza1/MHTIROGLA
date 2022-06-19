a9621af7e5c60f7c8f621cdd21e35258db1af819
김민규 <minq0302@naver.com>
2022-06-19 15:05:48 +0900

    Solved 11653

--- 2001~3000/2581 소수.py
+++ 2001~3000/2581 소수.py
@@ -6,8 +6,6 @@ sosu_list = []
 for i in range(m,n+1):
     if i == 1:
         pass
-    elif i == 2:
-        sosu_list.append(i)
     else:
         for j in range(2,i):
             if i % j == 0:
