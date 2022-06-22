28fbb66d6d3b22ee67d631dab43c49624e0cf2d3
cadenzza1 <minq0302@naver.com>
2022-06-22 18:01:43 +0900

    Not Solved 9020 - 무한출력 수정해야함!

---
+++ 9001~10000/9020 골드바흐의 추측.py
@@ -0,0 +1,21 @@
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2,int(num**0.5)+1):
+            if num % i == 0:
+                return False
+        else: return True
+
+for _ in range(int(input())): # 입력받은 수 만큼의 테스트케이스를 입력받는당
+    num = int(input())
+
+    a,b = num//2, num//2
+    while a > 0:
+        if isPrime(a) and isPrime(b):
+            print(a,b)
+            
+        else:
+            a -= 1
+            b += 1
+
