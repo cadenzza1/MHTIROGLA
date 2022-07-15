b1da6cea29f6db5eaf04ab3be19c6c5acabdea40
김민규 <minq0302@naver.com>
2022-07-15 19:58:42 +0900

    Solved 11651 sys.stdin.readline을 생활화하자

--- 11001~12000/11651 좌표 정렬하기2.py
+++ 11001~12000/11651 좌표 정렬하기2.py
@@ -1,8 +1,10 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 arr = []
 
 for i in range(n):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int,sys.stdin.readline().split())))
 
 arr.sort(key = lambda x : (x[1],x[0]))
 for x, y in arr:
