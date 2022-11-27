8cd0fc26021356d983ba7ba35808b2e6b393cfd2
김민규 <minq0302@naver.com>
2022-11-28 02:07:41 +0900

    Solved 4949 균형잡힌 세상

--- 4001~5000/4949 균형잡힌 세상.py
+++ 4001~5000/4949 균형잡힌 세상.py
@@ -1,26 +1,40 @@
-strings = input().split('.')
+strings = []
+while True:
+    string = input()
+    if string == '.':
+        break
+    else:
+        strings.append(string)
 
 for string in strings:
     stack = []
     for i in string:
         if i == ')':
-            if stack[-1] == '(':
-                stack.pop()
+            if stack:
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    print("no")
+                    break
             else:
-                print("NO")
+                print("no")
                 break
         elif i == '(':
             stack.append('(')
         elif i == ']':
-            if stack[-1] == '[':
-                stack.pop()
+            if stack:
+                if stack[-1] == '[':
+                    stack.pop()
+                else:
+                    print("no")
+                    break
             else:
-                print("NO")
+                print("no")
                 break
         elif i == '[':
             stack.append('[')
     else:
         if stack:
-            print("NO")
+            print("no")
         else:
-            print("YES")
+            print("yes")
