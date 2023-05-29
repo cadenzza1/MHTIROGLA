89b411d8f5012eec2c086b1a8ac51caf4a65a7d0
Kim mingyu <minq0302@naver.com>
2023-05-29 17:17:52 +0900

    프로그래머스 덧칠하기

---
+++ 프로그래머스/덧칠하기.py
@@ -0,0 +1,14 @@
+from collections import deque
+def solution(n, m, section):
+    dq = deque(section)
+    answer = 1
+    flag = section[0] + m - 1
+    
+    while dq:
+        if dq[0] <= flag:
+            dq.popleft()
+        else:
+            answer += 1
+            flag = dq[0] + m - 1
+            
+    return answer
