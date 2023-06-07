712f5021f821e6c82fbd1bcc4049bb66055e277f
mingyu Kim <81146131+cadenzza1@users.noreply.github.com>
2023-06-07 10:15:45 +0900

    1759 암호만들기

---
+++ 1001~2000/1759 암호만들기.py
@@ -0,0 +1,26 @@
+from itertools import permutations,combinations
+
+l,c = map(int,input().split())
+c_arr = sorted(list(input().split()))
+
+mo = ['a','e','i','o','u']
+
+result = []
+
+for i in combinations(c_arr,l):
+    mo_cnt = 0
+    ja_cnt = 0
+    for j in i:
+        if j in mo:
+            mo_cnt += 1
+        else:
+            ja_cnt += 1
+    i = list(i)
+    flag = i == sorted(i)
+    if mo_cnt >= 1 and ja_cnt >= 2 and flag:
+        print("".join(i))
+#         result.append("".join(i))
+
+# result.sort()
+# for i in result:
+#     print(i)
