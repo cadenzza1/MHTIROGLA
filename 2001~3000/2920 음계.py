b929808677d9aa0cf1b74c2871ba02cd8c7b3cb7
mingyu Kim <81146131+cadenzza1@users.noreply.github.com>
2023-06-28 06:40:15 +0900

    2920음계 정렬을 이용한 깔쌈한 풀이

--- 2001~3000/2920 음계.py
+++ 2001~3000/2920 음계.py
@@ -1,23 +1,8 @@
 li = list(map(int,input().split()))
 
-if li[0] == 1:
-    for i in range(1,len(li)):
-        if (li[i]-1) == li[i-1]:
-            continue
-        else:
-            print("mixed")
-            break
-    else:
-        print("ascending")
-elif li[0] == 8:
-    for i in range(1,len(li)):
-        if (li[i]+1) == li[i-1]:
-            continue
-        else:
-            print("mixed")
-            break
-    else:
-        print("descending")
+if li == sorted(li):
+    print("ascending")
+elif li == sorted(li,reverse=True):
+    print("descending")
 else:
     print("mixed")
-
