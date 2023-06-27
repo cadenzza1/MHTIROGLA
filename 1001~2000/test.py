4fc926eb6d6d8446432cc375ac1bab62c227f9b0
mingyu Kim <81146131+cadenzza1@users.noreply.github.com>
2023-06-28 06:25:19 +0900

    1931 회의실 배정

--- 1001~2000/test.py
+++ 1001~2000/test.py
@@ -1,17 +1,2 @@
-n = int(input())
-num = n
-cnt = 0
-
-while 1:
-    a = num // 10
-    b = num % 10
-    c = (a+b) % 10
-    num = (b*10) + c
-
-    cnt += 1
-    if num==n:
-        break
-
-print(cnt)
-
-print("For Everyday Commit")
+test = (lambda x,y : x+y)(2,5)
+print(test)
