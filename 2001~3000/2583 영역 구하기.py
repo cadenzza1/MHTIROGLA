89b1a70c82ee2dff191f8675a6be60551e23b519
mingyu Kim <81146131+cadenzza1@users.noreply.github.com>
2023-08-10 14:47:43 +0900

    2583 영역 구하기

--- 2001~3000/2583 영역 구하기.py
+++ 2001~3000/2583 영역 구하기.py
@@ -1,38 +1,44 @@
+import sys 
+sys.setrecursionlimit(10**7) # 최대 재귀 깊이 10,000,000으로 제한
+
 m,n,k = map(int,input().split())
 
 graph = [[0] * n for _ in range(m)]
 visited = [[False] * n for _ in range(m)]
 
-for i in range(k):
+for _ in range(k):
     x1,y1,x2,y2 = map(int,input().split())
-    for j in range(x1,x2):
-        for l in range(y1,y2):
-            graph[l][j] = 1
+    for i in range(y1,y2): # 2~4
+        for j in range(x1,x2): # 0~4
+            graph[i][j] = 1
 
 
 # 상하좌우 이동용
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-
+cnt = 0
 result = []
 
-def DFS(x,y,cnt):
+def DFS(x,y):
+    global cnt
     visited[x][y] = True
     cnt += 1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if (0<=nx<n) and (0<=ny<m):
-            if visited[ny][nx] == False:
-                if graph[ny][nx] == 0:
-                    DFS(ny,nx,cnt)
+        if (0<=nx<m) and (0<=ny<n):
+            if visited[nx][ny] == False:
+                if graph[nx][ny] == 0:
+                    DFS(nx,ny)
+    return cnt
 
 for i in range(m):
     for j in range(n):
-        cnt = 0
-        if visited[i][j] == False:
-            DFS(i,j,cnt)
-            result.append(cnt)
+        if visited[i][j] == False and graph[i][j] == 0:
+            result.append(DFS(i,j))
+            cnt = 0
 
 result.sort()
-print(result)
+print(len(result))
+for i in result:
+    print(i,end = ' ')
