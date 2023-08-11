cd9171047d2f6ed51fde2ecbde8db1336a38667c
mingyu Kim <81146131+cadenzza1@users.noreply.github.com>
2023-08-11 20:49:09 +0900

    1141 접두사 정답 ver

---
+++ 1001~2000/1141 접두사 정답.py
@@ -0,0 +1,16 @@
+n = int(input())
+words = [input() for _ in range(n)]
+
+words.sort(key=len)
+res = 0
+
+for i in range(n):
+    flag = False
+    for j in range(i+1,n):
+        if words[i] == words[j][:len(words[i])]:
+            flag = True
+
+    if flag != True:
+        res +=1
+
+print(res)
